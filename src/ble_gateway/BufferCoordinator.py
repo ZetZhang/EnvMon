6aee063ddd57f1c7286ef1d203d16a18dca40d06
ZetZhang <13660591402@163.com>
2022-05-23 22:38:03 +0800

    success send data message to server

---
+++ src/ble_gateway/BufferCoordinator.py
@@ -0,0 +1,110 @@
+import queue
+import json
+
+class DataQueue:
+    def __init__(self, max):
+        self.queueData = queue.Queue(max)
+
+    def pushData(self, key, value):
+        #  self.queueData.put_nowait({key: value})
+        self.queueData.put_nowait((key, value))
+
+    def popData(self):
+        #  return self.queueData.get_nowait()[key]
+        return self.queueData.get_nowait()
+
+    def takeData(self):
+        return self.popData()[1]
+
+    def takeKey(self):
+        return self.popData()[0]
+
+    def waitAllDone(self):
+        return self.queueData.join()
+
+class DataBuffer:
+    def __init__(self):
+        self.controlFlag = False
+        self.thresholdFlag = False
+        self.temperature = ""
+        self.humidity = ""
+        self.pressure = ""
+        self.sample = ""
+        self.lightIntensity = ""
+        self.controlNotice = ""
+        self.threshold = ""
+
+    def isSensorDataReady(self):
+        return self.temperature != "" and self.humidity != "" and self.pressure != "" and self.sample != "" and self.lightIntensity
+
+    def sensorDataReset(self):
+        self.temperature = ""
+        self.humidity = ""
+        self.pressure = ""
+        self.lightIntensity = ""
+
+    def setTemperature(self, value):
+        if isinstance(value, bytes):
+            self.temperature = str(value)
+
+    def setHumidity(self, value):
+        if isinstance(value, bytes):
+            self.humidity = str(value)
+
+    def setPressure(self, value):
+        if isinstance(value, bytes):
+            self.pressure = str(value)
+
+    def setSample(self, value):
+        if isinstance(value, bytes):
+            self.sample = str(value)
+
+    def setLightIntensity(self, value):
+        if isinstance(value, bytes):
+            self.lightIntensity = str(value)
+
+    def setControlNotice(self, value):
+        self.controlFlag = True
+        if isinstance(value, bytes):
+            self.controlNotice = str(value)
+
+    def setThreshold(self, value):
+        self.thresholdFlag = True
+        if isinstance(value, bytes):
+            self.threshold = str(value)
+
+    def getSensorJsonData(self):
+        if self.isSensorDataReady():
+            data = [{'temperature': self.temperature},
+                    {'humidity': self.humidity},
+                    {'pressure': self.humidity},
+                    {'lightintensity': self.lightIntensity},
+                    {'controlnotice': self.controlNotice},
+                    {'threshold': self.threshold}]
+
+            jsonData = json.dumps(data)
+            return jsonData
+        return None
+
+    def getControlJsonData(self):
+        if self.controlNotice == "":
+            return None
+        data = {'controlNotice': self.controlNotice}
+        jsonData = json.dumps(data)
+        self.controlNotice = ""
+        return jsonData
+
+    def getThresholdJsonData(self):
+        if self.threshold == "":
+            return None
+        data = {'thresholdList': self.threshold}
+        jsonData = json.dumps(data)
+        self.threshold = ""
+        return jsonData
+
+    def getJsonData(self):
+        data = {'control': self.getControlJsonData(),
+                'threshold': self.getThresholdJsonData(),
+                'sensor': self.getSensorJsonData()}
+        jsonData = json.dumps(data)
+        return jsonData
--- src/ble_gateway/ble_gateway.py
+++ src/ble_gateway/ble_gateway.py
@@ -7,6 +7,11 @@ from tornado.ioloop import IOLoop, PeriodicCallback
 from tornado import gen
 from tornado.websocket import websocket_connect
 
+import BufferCoordinator
+
+dataQueue = BufferCoordinator.DataQueue(10)
+dataBuffer = BufferCoordinator.DataBuffer()
+
 class ReceiveDelegate(DefaultDelegate):
     def __init__(self, peripheral):
         DefaultDelegate.__init__(self)
@@ -18,31 +23,45 @@ class ReceiveDelegate(DefaultDelegate):
         #          self.peripheral.getTemperatureHandle(): lambda data: print("yes"),
         #  } [cHandle](data)
         if cHandle == self.peripheral.getTemperatureHandle():
+            #  dataQueue.pushData('temperature', data)
+            dataBuffer.setTemperature(data)
             [temperature] = struct.unpack('f', data)
             print("temperature data:", round(temperature, 2), "°C")
         elif cHandle == self.peripheral.getHumidityHandle():
+            #  dataQueue.pushData('humidity', data)
+            dataBuffer.setHumidity(data)
             [humidity] = struct.unpack('f', data)
             print("humidity data:", round(humidity, 2), "%")
         elif cHandle == self.peripheral.getPressureHandle():
+            #  dataQueue.pushData('pressure', data)
+            dataBuffer.setPressure(data)
             [pressure] = struct.unpack('f', data)
             print("pressure data:", round(pressure, 2), "kPA")
         elif cHandle == self.peripheral.getSampleHandle():
+            #  dataQueue.pushData('sample', data)
+            dataBuffer.setSample(data)
             [sample] = struct.unpack('b', data)
             print("sample data:", sample, "dB")
         elif cHandle == self.peripheral.getLightIntensityHandle():
+            #  dataQueue.pushData('lightintensity', data)
+            dataBuffer.setLightIntensity(data)
             [lightIntensity] = struct.unpack('b', data)
             print("light intensity data:", lightIntensity, "steps")
         elif cHandle == self.peripheral.getControlNoticeHandle():
+            #  dataQueue.pushData('controlnotice', data)
+            dataBuffer.setControlNotice(data)
             [control] = struct.unpack('b', data)
             lightStr = "on" if control & 1 else "off"
             lightControlStr = "on" if control & 2 else "off"
             smartReminderStr = "on" if control & 4 else "off"
             print("control data: [ light:", lightStr, ", light control:", lightControlStr, ", smart reminder:", smartReminderStr, "]")
         elif cHandle == self.peripheral.getThresholdHandle():
+            #  dataQueue.pushData('threshold', data)
+            dataBuffer.setThreshold(data)
             thresholdList = list(data)
             print("threshold data list:", thresholdList)
 
-class wsConnect(object):
+class wsConnect():
     def __init__(self, url, timeout):
         self.count = 0
         self.url = url
@@ -68,14 +87,20 @@ class wsConnect(object):
     def run(self):
         while True:
             self.count += 1
-            yield self.ws.write_message('{}'.format(self.count))
+            jsonData = dataBuffer.getJsonData()
+            print(jsonData)
+            #  yield self.ws.write_message('mes {}'.format(self.count))
+            yield self.ws.write_message('mes {}: {}'.format(self.count, jsonData))
+
             msg = yield self.ws.read_message()
+
             if msg is None:
                 print("connection closed")
                 self.ws = None
                 break
+
             print('message: {}'.format(msg))
-            yield gen.sleep(2)
+            yield gen.sleep(1)
 
     def keep_alive(self):
         if self.ws is None:
@@ -83,8 +108,7 @@ class wsConnect(object):
         else:
             self.ws.write_message("keep alive")
 
-def sensor_acquisition_service(name, peripheral):
-    print("{} threading running.".format(name))
+def sensor_acquisition_service():
     peripheral = BleSense.PeripheralService()
     peripheral.notifyHandler(ReceiveDelegate)
     peripheral.notificationEnable()
@@ -92,10 +116,19 @@ def sensor_acquisition_service(name, peripheral):
 
 if __name__ == '__main__':
     try:
-        pService = 0
-        sThread = threading.Thread(target=sensor_acquisition_service, args=('mas', pService))
+        sThread = threading.Thread(target=sensor_acquisition_service)
 
+        print("sensor acquisition threading running...")
         sThread.start()
+
+        try:
+            dataQueue.pushBuffer('temperature', 2)
+            dataQueue.pushBuffer('temperature', 2)
+            dataQueue.pushBuffer('temperature', 2)
+            print(dataQueue.popBuffer('temperature'))
+            print(dataQueue.popBuffer('temperature'))
+        except:
+            print("data queue error")
 
         print("connting...")
         wsConnect("ws://localhost:12340/echo", 5)
