4619b42790fdd799c29b63f60b6295d1ef48c5a1
ZetZhang <13660591402@163.com>
2022-05-23 16:12:43 +0800

    tronado

---
+++ src/ble_gateway/BleSense.py
@@ -0,0 +1,131 @@
+from bluepy.btle import Scanner, DefaultDelegate,Peripheral
+
+CCCD_UUID = 0x2902
+TEMPERATURE_SERVICE_UUID = "fe2c0000-0730-4a71-b132-4917c2bb832d"
+HUMIDITY_SERVICE_UUID = "b1490000-5b47-44d0-88fd-5397b5511263"
+PRESSURE_SERVICE_UUID = "41ec0000-6818-4108-80e8-82bd95504b7e"
+SAMPLE_SERVICE_UUID = "855f0000-9f0f-49b7-88f7-d7f66145f461"
+LIGHT_INTENSITY_SERVICE_UUID = "b4c20000-8684-4ba3-b784-e0b4a499a042"
+CONTROL_SERVICE_UUID = "6c880000-6ca3-4775-9b56-c6ac4d0c1f72"
+SENSOR_THRESHOLD_SERVICE_UUID = "75d40000-e036-4297-bea3-d9ea16d570e4"
+
+TEMPERATURE_CHARACTERISTIC_UUID = "fe2c0001-0730-4a71-b132-4917c2bb832d"
+HUMIDITY_CHARACTERISTIC_UUID = "b1490001-5b47-44d0-88fd-5397b5511263"
+PRESSURE_CHARACTERISTIC_UUID = "41ec0001-6818-4108-80e8-82bd95504b7e"
+SAMPLE_CHARACTERISTIC_UUID = "855f0001-9f0f-49b7-88f7-d7f66145f461"
+LIGHT_INTENSITY_CHARACTERISTIC_UUID = "b4c20001-8684-4ba3-b784-e0b4a499a042"
+CONTROL_NOTICE_CHARACTERISTIC_UUID = "6c880001-6ca3-4775-9b56-c6ac4d0c1f72"
+SENSOR_THRESHOLD_CHARACTERISTIC_UUID = "75d40001-e036-4297-bea3-d9ea16d570e4"
+
+class ScanDelegate(DefaultDelegate):
+    def __init__(self):
+        DefaultDelegate.__init__(self)
+
+    def handleDiscovery(self, dev, isNewDev, isNewData):
+        if isNewDev:
+            print("Discovered device {}".format(dev.addr))
+        elif isNewData:
+            print("Recevied new data from {}".format(dev.addr))
+
+class PeripheralService:
+    device_addr = "f6:84:22:8e:62:4a"
+
+    def __init__(self):
+        self.peripheral = Peripheral(self.device_addr)
+        self.peripheral.setMTU(300)
+
+        self.controlChangedFromServer = False
+        self.thresholdChangedFromServer = False
+
+        self.cData = bytes([0])
+        self.tInfo = bytes([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+        self.tempService = self.peripheral.getServiceByUUID(TEMPERATURE_SERVICE_UUID) 
+        self.humidService = self.peripheral.getServiceByUUID(HUMIDITY_SERVICE_UUID)
+        self.pressService = self.peripheral.getServiceByUUID(PRESSURE_SERVICE_UUID)
+        self.sampService = self.peripheral.getServiceByUUID(SAMPLE_SERVICE_UUID)
+        self.lightService = self.peripheral.getServiceByUUID(LIGHT_INTENSITY_SERVICE_UUID)
+        self.controlService = self.peripheral.getServiceByUUID(CONTROL_SERVICE_UUID)
+        self.thresholdService = self.peripheral.getServiceByUUID(SENSOR_THRESHOLD_SERVICE_UUID)
+
+        self.tempCharacteristic = self.tempService.getCharacteristics(TEMPERATURE_CHARACTERISTIC_UUID)[0]
+        self.humidCharacteristic = self.humidService.getCharacteristics(HUMIDITY_CHARACTERISTIC_UUID)[0]
+        self.pressCharacteristic = self.pressService.getCharacteristics(PRESSURE_CHARACTERISTIC_UUID)[0]
+        self.sampCharacteristic = self.sampService.getCharacteristics(SAMPLE_CHARACTERISTIC_UUID)[0]
+        self.lightIntensityCharacteristic = self.lightService.getCharacteristics(LIGHT_INTENSITY_CHARACTERISTIC_UUID)[0]
+        self.controlNoticeCharacteristic = self.controlService.getCharacteristics(CONTROL_NOTICE_CHARACTERISTIC_UUID)[0]
+        self.sensorThresholdCharacteristic = self.thresholdService.getCharacteristics(SENSOR_THRESHOLD_CHARACTERISTIC_UUID)[0]
+
+    def getTemperatureHandle(self):
+        return self.tempCharacteristic.getHandle()
+
+    def getHumidityHandle(self):
+        return self.humidCharacteristic.getHandle()
+
+    def getPressureHandle(self):
+        return self.pressCharacteristic.getHandle()
+
+    def getSampleHandle(self):
+        return self.sampCharacteristic.getHandle()
+
+    def getLightIntensityHandle(self):
+        return self.lightIntensityCharacteristic.getHandle()
+
+    def getControlNoticeHandle(self):
+        return self.controlNoticeCharacteristic.getHandle()
+
+    def getThresholdHandle(self):
+        return self.sensorThresholdCharacteristic.getHandle()
+
+    def disconnect(self):
+        self.disconnect()
+
+    def notifyHandler(self, cb):
+        self.peripheral.setDelegate(cb(self))
+        self.cb_backup = cb
+
+    def sendControlData(self, data):
+        response = self.controlNoticeCharacteristic.write(bytes([3]), withResponse=True)
+        self.controlChangedFromServer = False
+        print(response)
+
+    def sendThresholdInfo(self, info):
+        response = self.sensorThresholdCharacteristic.write(info, withResponse=True)
+        self.thresholdChangedFromServer = False
+        print(response)
+
+    def isConnected(self):
+        try:
+            return self.peripheral.getState() == 'conn'
+        except:
+            return False
+
+    def connect(self):
+        if (self.isConnected() == False):
+            print("reconnecting")
+            self.peripheral.connect(self.device_addr)
+            self.peripheral.setDelegate(self.cb_backup(self))
+            self.notificationEnable()
+            self.indicationEnable()
+        return True
+
+    def notificationEnable(self):
+        self.tempCharacteristic.getDescriptors(forUUID=CCCD_UUID)[0].write(bytes([1, 0]))
+        self.humidCharacteristic.getDescriptors(forUUID=CCCD_UUID)[0].write(bytes([1, 0]))
+        self.pressCharacteristic.getDescriptors(forUUID=CCCD_UUID)[0].write(bytes([1, 0]))
+        self.sampCharacteristic.getDescriptors(forUUID=CCCD_UUID)[0].write(bytes([1, 0]))
+        self.lightIntensityCharacteristic.getDescriptors(forUUID=CCCD_UUID)[0].write(bytes([1, 0]))
+        self.controlNoticeCharacteristic.getDescriptors(forUUID=CCCD_UUID)[0].write(bytes([1, 0]))
+        self.sensorThresholdCharacteristic.getDescriptors(forUUID=CCCD_UUID)[0].write(bytes([1, 0]))
+
+    def indicationEnable(self):
+        pass
+
+    def loop(self):
+        while self.connect():
+            if self.peripheral.waitForNotifications(1.0):
+                continue
+            if self.controlChangedFromServer:
+                self.sendControlData(self.cData)
+            if self.thresholdChangedFromServer:
+                self.sendThresholdInfo(self.tInfo)
--- src/ble_gateway/ble_gateway.py
+++ src/ble_gateway/ble_gateway.py
@@ -1,33 +1,11 @@
-from bluepy.btle import Scanner, DefaultDelegate,Peripheral
-import struct
+from bluepy.btle import DefaultDelegate
+import BleSense
 import threading
+import struct
 
-CCCD_UUID = 0x2902
-TEMPERATURE_SERVICE_UUID = "fe2c0000-0730-4a71-b132-4917c2bb832d"
-HUMIDITY_SERVICE_UUID = "b1490000-5b47-44d0-88fd-5397b5511263"
-PRESSURE_SERVICE_UUID = "41ec0000-6818-4108-80e8-82bd95504b7e"
-SAMPLE_SERVICE_UUID = "855f0000-9f0f-49b7-88f7-d7f66145f461"
-LIGHT_INTENSITY_SERVICE_UUID = "b4c20000-8684-4ba3-b784-e0b4a499a042"
-CONTROL_SERVICE_UUID = "6c880000-6ca3-4775-9b56-c6ac4d0c1f72"
-SENSOR_THRESHOLD_SERVICE_UUID = "75d40000-e036-4297-bea3-d9ea16d570e4"
-
-TEMPERATURE_CHARACTERISTIC_UUID = "fe2c0001-0730-4a71-b132-4917c2bb832d"
-HUMIDITY_CHARACTERISTIC_UUID = "b1490001-5b47-44d0-88fd-5397b5511263"
-PRESSURE_CHARACTERISTIC_UUID = "41ec0001-6818-4108-80e8-82bd95504b7e"
-SAMPLE_CHARACTERISTIC_UUID = "855f0001-9f0f-49b7-88f7-d7f66145f461"
-LIGHT_INTENSITY_CHARACTERISTIC_UUID = "b4c20001-8684-4ba3-b784-e0b4a499a042"
-CONTROL_NOTICE_CHARACTERISTIC_UUID = "6c880001-6ca3-4775-9b56-c6ac4d0c1f72"
-SENSOR_THRESHOLD_CHARACTERISTIC_UUID = "75d40001-e036-4297-bea3-d9ea16d570e4"
-
-class ScanDelegate(DefaultDelegate):
-    def __init__(self):
-        DefaultDelegate.__init__(self)
-
-    def handleDiscovery(self, dev, isNewDev, isNewData):
-        if isNewDev:
-            print("Discovered device {}".format(dev.addr))
-        elif isNewData:
-            print("Recevied new data from {}".format(dev.addr))
+from tornado.ioloop import IOLoop, PeriodicCallback
+from tornado import gen
+from tornado.websocket import websocket_connect
 
 class ReceiveDelegate(DefaultDelegate):
     def __init__(self, peripheral):
@@ -64,121 +42,63 @@ class ReceiveDelegate(DefaultDelegate):
             thresholdList = list(data)
             print("threshold data list:", thresholdList)
 
-class PeripheralService:
-    device_addr = "f6:84:22:8e:62:4a"
+class wsConnect(object):
+    def __init__(self, url, timeout):
+        self.count = 0
+        self.url = url
+        self.timeout = timeout
+        self.ioloop = IOLoop.instance()
+        self.ws = None
+        self.connect()
+        PeriodicCallback(self.keep_alive, 20000).start()
+        self.ioloop.start()
 
-    def __init__(self):
-        self.peripheral = Peripheral(self.device_addr)
-        self.peripheral.setMTU(300)
-
-        self.controlChangedFromServer = False
-        self.thresholdChangedFromServer = False
-
-        self.cData = bytes([0])
-        self.tInfo = bytes([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-
-        self.tempService = self.peripheral.getServiceByUUID(TEMPERATURE_SERVICE_UUID) 
-        self.humidService = self.peripheral.getServiceByUUID(HUMIDITY_SERVICE_UUID)
-        self.pressService = self.peripheral.getServiceByUUID(PRESSURE_SERVICE_UUID)
-        self.sampService = self.peripheral.getServiceByUUID(SAMPLE_SERVICE_UUID)
-        self.lightService = self.peripheral.getServiceByUUID(LIGHT_INTENSITY_SERVICE_UUID)
-        self.controlService = self.peripheral.getServiceByUUID(CONTROL_SERVICE_UUID)
-        self.thresholdService = self.peripheral.getServiceByUUID(SENSOR_THRESHOLD_SERVICE_UUID)
-
-        self.tempCharacteristic = self.tempService.getCharacteristics(TEMPERATURE_CHARACTERISTIC_UUID)[0]
-        self.humidCharacteristic = self.humidService.getCharacteristics(HUMIDITY_CHARACTERISTIC_UUID)[0]
-        self.pressCharacteristic = self.pressService.getCharacteristics(PRESSURE_CHARACTERISTIC_UUID)[0]
-        self.sampCharacteristic = self.sampService.getCharacteristics(SAMPLE_CHARACTERISTIC_UUID)[0]
-        self.lightIntensityCharacteristic = self.lightService.getCharacteristics(LIGHT_INTENSITY_CHARACTERISTIC_UUID)[0]
-        self.controlNoticeCharacteristic = self.controlService.getCharacteristics(CONTROL_NOTICE_CHARACTERISTIC_UUID)[0]
-        self.sensorThresholdCharacteristic = self.thresholdService.getCharacteristics(SENSOR_THRESHOLD_CHARACTERISTIC_UUID)[0]
-
-    def getTemperatureHandle(self):
-        return self.tempCharacteristic.getHandle()
-
-    def getHumidityHandle(self):
-        return self.humidCharacteristic.getHandle()
-
-    def getPressureHandle(self):
-        return self.pressCharacteristic.getHandle()
-
-    def getSampleHandle(self):
-        return self.sampCharacteristic.getHandle()
-
-    def getLightIntensityHandle(self):
-        return self.lightIntensityCharacteristic.getHandle()
-
-    def getControlNoticeHandle(self):
-        return self.controlNoticeCharacteristic.getHandle()
-
-    def getThresholdHandle(self):
-        return self.sensorThresholdCharacteristic.getHandle()
-
-    def disconnect(self):
-        self.disconnect()
-
-    def notifyHandler(self, cb):
-        self.peripheral.setDelegate(cb(self))
-        self.cb_backup = cb
-
-    def sendControlData(self, data):
-        response = self.controlNoticeCharacteristic.write(bytes([3]), withResponse=True)
-        self.controlChangedFromServer = False
-        print(response)
-
-    def sendThresholdInfo(self, info):
-        response = self.sensorThresholdCharacteristic.write(info, withResponse=True)
-        self.thresholdChangedFromServer = False
-        print(response)
-
-    def isConnected(self):
-        try:
-            return self.peripheral.getState() == 'conn'
-        except:
-            return False
-
+    @gen.coroutine
     def connect(self):
-        if (self.isConnected() == False):
-            print("reconnecting")
-            self.peripheral.connect(self.device_addr)
-            self.peripheral.setDelegate(self.cb_backup(self))
-            self.notificationEnable()
-            self.indicationEnable()
-        return True
+        #  print("trying to connect")
+        try:
+            self.ws = yield websocket_connect(self.url)
+        except:
+            print("connection error")
+        else:
+            print("conncted")
+            self.run()
 
-    def notificationEnable(self):
-        self.tempCharacteristic.getDescriptors(forUUID=CCCD_UUID)[0].write(bytes([1, 0]))
-        self.humidCharacteristic.getDescriptors(forUUID=CCCD_UUID)[0].write(bytes([1, 0]))
-        self.pressCharacteristic.getDescriptors(forUUID=CCCD_UUID)[0].write(bytes([1, 0]))
-        self.sampCharacteristic.getDescriptors(forUUID=CCCD_UUID)[0].write(bytes([1, 0]))
-        self.lightIntensityCharacteristic.getDescriptors(forUUID=CCCD_UUID)[0].write(bytes([1, 0]))
-        self.controlNoticeCharacteristic.getDescriptors(forUUID=CCCD_UUID)[0].write(bytes([1, 0]))
-        self.sensorThresholdCharacteristic.getDescriptors(forUUID=CCCD_UUID)[0].write(bytes([1, 0]))
+    @gen.coroutine
+    def run(self):
+        while True:
+            self.count += 1
+            yield self.ws.write_message('{}'.format(self.count))
+            msg = yield self.ws.read_message()
+            if msg is None:
+                print("connection closed")
+                self.ws = None
+                break
+            print('message: {}'.format(msg))
+            yield gen.sleep(2)
 
-    def indicationEnable(self):
-        pass
-
-    def loop(self):
-        while self.connect():
-            if self.peripheral.waitForNotifications(1.0):
-                continue
-            if self.controlChangedFromServer:
-                self.sendControlData(self.cData)
-            if self.thresholdChangedFromServer:
-                self.sendThresholdInfo(self.tInfo)
+    def keep_alive(self):
+        if self.ws is None:
+            self.connect()
+        else:
+            self.ws.write_message("keep alive")
 
 def sensor_acquisition_service(name, peripheral):
     print("{} threading running.".format(name))
+    peripheral = BleSense.PeripheralService()
     peripheral.notifyHandler(ReceiveDelegate)
     peripheral.notificationEnable()
     peripheral.loop()
 
 if __name__ == '__main__':
-    pService = PeripheralService()
     try:
+        pService = 0
         sThread = threading.Thread(target=sensor_acquisition_service, args=('mas', pService))
 
         sThread.start()
+
+        print("connting...")
+        wsConnect("ws://localhost:12340/echo", 5)
 
     except:
         print("start thread errro...")
